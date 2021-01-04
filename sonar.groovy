589c94f66836a61aeb013565eb8bb24ea6736f97
tejasrik <65765135+tejasrik@users.noreply.github.com>
2021-01-04 16:08:55 +0530

    Update sonar.groovy

--- sonar.groovy
+++ sonar.groovy
@@ -1,8 +1,8 @@
 def call(){
-stage('SonarQube Analysis'){
+
   def mavenHome = tool name: "Maven-3.6.3",type: "maven"
   withSonarQubeEnv('sonar-6') {
   sh "${mavenHome}/bin/mvn sonar:sonar"
  }
 }
-}
+
